3924dabd171a2c9369beb4f04403ff34c5bdf3b6
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-25 18:17:41 +0100

    created addNums10 and addManyNums10

--- phase-2.js
+++ phase-2.js
@@ -2,13 +2,21 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let results = [];
+  for (let i = 1; i <= 10; i++) {
+    results.push(addNums(i * increment));
+  }
+  return results;
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let results = [];
+  for (let i = 1; i <= 10; i++) {
+    results.push(addManyNums(i * increment));
+  }
+  return results;
 
 }
 
